4117f528bfa6e27ff5642fb86cd714538def0180
Roman Oks <advenua@gmail.com>
2022-08-04 21:49:24 +0300

    Added default folder creation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
   has_many :notes, dependent: :destroy
   has_many :folders, dependent: :destroy
+
+  after_create do
+    self.folders.create(name: "Все записи", default: true)
+  end
 end
