a2aa268195dc322fe091ae697878c411664fec64
Roman Oks <advenua@gmail.com>
2022-08-04 19:05:14 +0300

    Added initial seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create(email: "test@test.com", password: "123123", name: "Тестовый аккаунт")
+
+user.notes.create(
+  title: "PCI Express",
+  tag_list: "шина, передача данных",
+  content:
+    "<div>" \
+    "В отличие от стандарта PCI, использовавшего для передачи данных общую шину с подключением параллельно" \
+    "нескольких устройств, PCI Express, в общем случае, является пакетной сетью с топологией типа звезда." \
+    "<br><br>" \
+    "Устройства PCI Express взаимодействуют между собой через среду, образованную коммутаторами, при этом" \
+    "каждое устройство напрямую связано соединением типа точка-точка с коммутатором." \
+    "</div>"
+)
